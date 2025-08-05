819ad5c1ca31df2fe2858c2287ae1b57df07483e
Glory-Day <alwaysgloryday@gmail.com>
2025-08-05 11:55:39 +0900

    Feature: Variable fetchs
    - Screen resolution
    - Looping BGM
    - Navigation Panel

---
+++ FBWG/Assets/Scripts/Object/ObjectToggle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Backend.Object
+{
+    public class ObjectToggle : CollisionStayedDetection
+    {
+        [Header("Game Object Settings")]
+        [SerializeField] private GameObject instance;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var component = other.GetComponent<ObjectIdentifier>();
+            if (type.HasFlag(component.type) == false)
+            {
+                return;
+            }
+            
+            instance?.SetActive(true);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            var component = other.GetComponent<ObjectIdentifier>();
+            if (type.HasFlag(component.type) == false)
+            {
+                return;
+            }
+            
+            instance?.SetActive(false);
+        }
+    }
+}
